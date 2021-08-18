6b8aafa2ad52b1b8bec0961e4bf7e333d9251753
Hugo <dev@lexugo.ca>
2021-08-18 16:23:47 -0400

    Pipi Caca · Le jeu de la mort
    - Add search page
    - Add query param for search
    - Create useParam hook

--- components/search/index.js
+++ components/search/index.js
@@ -1,16 +1,24 @@
 import algoliasearch from 'algoliasearch/lite'
 import { InstantSearch } from 'react-instantsearch-dom'
 
+import { useRouter } from 'next/router'
+
 import Input from './search'
 import Hits from './hits'
+
+import useParam from 'hooks/useParam'
 
 const searchClient = algoliasearch("RI142KT8JI", "025962cc3a90d4f60d0aba72b1b312ed")
 
 function Search() {
+    const [query, setQuery] = useParam('q')
+
     return (
         <InstantSearch 
             searchClient={searchClient} 
             indexName="Ressource"
+            onSearchStateChange={({ query }) => setQuery(query)}
+            searchState={{query}}
         >
             <Input />
             <Hits />
---
+++ hooks/useParam.js
@@ -0,0 +1,19 @@
+import { useRouter } from "next/router"
+
+export default function useParam(parameter) {
+    const { query, pathname, replace } = useRouter()
+
+    function toUrl(value) {
+        if (value)
+            return { query: { ...query, [parameter]: value }} 
+        
+        const { [parameter]: _, ...params } = query // Remove param form url when undefined
+        return { pathname, query: params }
+    }
+
+    function set(value) {
+        replace(toUrl(value), undefined, { shallow: true })
+    }
+
+    return [query[parameter], set]
+}
