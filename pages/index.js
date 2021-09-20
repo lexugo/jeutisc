107290a6d805acff1e385e549de14d3ba8276ece
Hugo <dev@lexugo.ca>
2021-09-20 16:15:08 -0400

    Psychic Lemon · White Light
    - Style ressources on mobile

--- pages/index.js
+++ pages/index.js
@@ -25,49 +25,49 @@ function Home({ categories }) {
               <h2>I want to understand more about</h2>
               <div className='categories'>
                 <a href='#' className='category'>
-                  <input type='checkbox' id='1' name='category' value='Réalités Autochtones'/>
+                  <input type='radio' id='1' name='category' value='Réalités Autochtones'/>
                   <label htmlFor='1'>Réalités Autochtones</label>
                 </a>
                 <a href='#' className='category'>
-                  <input type='checkbox' id='2' name='category' value='Environement'/>
+                  <input type='radio' id='2' name='category' value='Environement'/>
                   <label htmlFor='2'>Environement</label>
                 </a>
                 <a href='#' className='category'>
-                  <input type='checkbox' id='3' name='category' value='Immigration'/>
+                  <input type='radio' id='3' name='category' value='Immigration'/>
                   <label htmlFor='3'>Immigration</label>
                 </a>
                 <a href='#' className='category'>
-                  <input type='checkbox' id='4' name='category' value='Environement'/>
+                  <input type='radio' id='4' name='category' value='Environement'/>
                   <label htmlFor='4'>Environement</label>
                 </a>
                 <a href='#' className='category'>
-                  <input type='checkbox' id='5' name='category' value='Racism'/>
+                  <input type='radio' id='5' name='category' value='Racism'/>
                   <label htmlFor='5'>Racism</label>
                   <div className='subcategories'>
                     <a href='#' className='subcategory'>
-                      <input type='checkbox' id='11' name='subcategory' value='Test'/>
+                      <input type='radio' id='11' name='subcategory' value='Test'/>
                       <label htmlFor='11'>Test</label>
                     </a>
                     <a href='#' className='subcategory'>
-                      <input type='checkbox' id='22' name='subcategory' value='Crise Climatique'/>
+                      <input type='radio' id='22' name='subcategory' value='Crise Climatique'/>
                       <label htmlFor='22'>Crise Climatique</label>
                     </a>
                     <a href='#' className='subcategory'>
-                      <input type='checkbox' id='33' name='subcategory' value='Fin du monde'/>
+                      <input type='radio' id='33' name='subcategory' value='Fin du monde'/>
                       <label htmlFor='33'>Fin du monde</label>
                     </a>
                     <a href='#' className='subcategory'>
-                      <input type='checkbox' id='44' name='subcategory' value='Sexualité'/>
+                      <input type='radio' id='44' name='subcategory' value='Sexualité'/>
                       <label htmlFor='44'>Sexualité</label>
                     </a>
                   </div>
                 </a>
                 <a href='#' className='category'>
-                  <input type='checkbox' id='6' name='category' value='Feminisme'/>
+                  <input type='radio' id='6' name='category' value='Feminisme'/>
                   <label htmlFor='6'>Feminisme</label>
                 </a>
                 <a href='#' className='category'>
-                  <input type='checkbox' id='7' name='category' value='Réalités Autochtones'/>
+                  <input type='radio' id='7' name='category' value='Réalités Autochtones'/>
                   <label htmlFor='7'>Réalités Autochtones</label>
                 </a>
               </div>
@@ -121,6 +121,46 @@ function Home({ categories }) {
                   <input id='inclusivité' type='checkbox' name='tags' value='pratique inclusive' />
                   <label htmlFor='inclusivité'>Pratique inclusive</label>
                 </div>
+                <div className='tag'>
+                  <input type='checkbox' id='canada' name='tags' value='canda' />
+                  <label htmlFor='canada'>Canada</label>
+                </div>
+                <div className='tag'>
+                  <input id='pensionnats' type='checkbox' name='tags' value='pensionats' />
+                  <label htmlFor='pensionnats'>Pensionnats autochtones</label>
+                </div>
+                <div className='tag'>
+                  <input id='climat' type='checkbox' name='tags' value='justice' />
+                  <label htmlFor='climat'>Justice climatique</label>
+                </div>
+                <div className='tag'>
+                  <input id='unsure' type='checkbox' name='tags' value='racisme' />
+                  <label htmlFor='unsure'>Racisme environnental</label>
+                </div>
+                <div className='tag'>
+                  <input id='inclusivité' type='checkbox' name='tags' value='pratique inclusive' />
+                  <label htmlFor='inclusivité'>Pratique inclusive</label>
+                </div>
+                <div className='tag'>
+                  <input type='checkbox' id='canada' name='tags' value='canda' />
+                  <label htmlFor='canada'>Canada</label>
+                </div>
+                <div className='tag'>
+                  <input id='pensionnats' type='checkbox' name='tags' value='pensionats' />
+                  <label htmlFor='pensionnats'>Pensionnats autochtones</label>
+                </div>
+                <div className='tag'>
+                  <input id='climat' type='checkbox' name='tags' value='justice' />
+                  <label htmlFor='climat'>Justice climatique</label>
+                </div>
+                <div className='tag'>
+                  <input id='unsure' type='checkbox' name='tags' value='racisme' />
+                  <label htmlFor='unsure'>Racisme environnental</label>
+                </div>
+                <div className='tag'>
+                  <input id='inclusivité' type='checkbox' name='tags' value='pratique inclusive' />
+                  <label htmlFor='inclusivité'>Pratique inclusive</label>
+                </div>
               </div>
             </div>
             <footer>
@@ -128,7 +168,144 @@ function Home({ categories }) {
               <button id='reset' type='reset'>x</button>
             </footer>
           </form>
+          <nav className='ressources'>
+            <div className='ressource'>
+              <span className='media'>
+                <span className='type'>Lexique</span>
+                <a href='#'>osensenparler.ca/covid-19+testing</a>
+              </span>
+              <h4 className='title'><a href='#'>Premières nations, Inuit et Métis</a></h4>
+              <span className='source'>Mikana et Amnistie Internationale</span>
+              <p className='description'>
+                Le terme Autochtone désigne une diversité de nations et de peuples ancrés dans les territoires qu'ils occupent depuis des millénaires.
+                Leur histoires se rejoingne...
+              </p>
+            </div>
+            <div className='ressource'>
+              <span className='media'>
+                <span className='type'>Balado</span>
+                <a href='#'>osensenparler.ca</a>
+              </span>
+              <h4 className='title'><a href='#'>Oser s'en parler</a></h4>
+              <span className='source'>Charlotte Côté</span>
+              <p className='description'>
+                Balado pour allochtones sur la réalité des peuples autochtones vivant au soit-disant Canada.
+              </p>
+            </div>
+            <div className='ressource'>
+              <span className='media'>
+                <span className='type'>Site</span>
+                <a href='#'>mikanna.ca</a>
+              </span>
+              <h4 className='title'><a href='#'>Mikana</a></h4>
+              <p className='description'>
+                Site oeuvrant à la sensibilisation sur les enjeux autochtones.
+              </p>
+            </div>
+            <div className='ressource'>
+              <span className='media'>
+                <span className='type'>Lexique</span>
+                <a href='#'>osensenparler.ca/covid-19+testing</a>
+              </span>
+              <h4 className='title'><a href='#'>Premières nations, Inuit et Métis</a></h4>
+              <span className='source'>Mikana et Amnistie Internationale</span>
+              <p className='description'>
+                Le terme Autochtone désigne une diversité de nations et de peuples ancrés dans les territoires qu'ils occupent depuis des millénaires.
+                Leur histoires se rejoingne...
+              </p>
+            </div>
+            <div className='ressource'>
+              <span className='media'>
+                <span className='type'>Balado</span>
+                <a href='#'>osensenparler.ca</a>
+              </span>
+              <h4 className='title'><a href='#'>Oser s'en parler</a></h4>
+              <span className='source'>Charlotte Côté</span>
+              <p className='description'>
+                Balado pour allochtones sur la réalité des peuples autochtones vivant au soit-disant Canada.
+              </p>
+            </div>
+            <div className='ressource'>
+              <span className='media'>
+                <span className='type'>Site</span>
+                <a href='#'>mikanna.ca</a>
+              </span>
+              <h4 className='title'><a href='#'>Mikana</a></h4>
+              <p className='description'>
+                Site oeuvrant à la sensibilisation sur les enjeux autochtones.
+              </p>
+            </div>
+            <div className='ressource'>
+              <span className='media'>
+                <span className='type'>Lexique</span>
+                <a href='#'>osensenparler.ca/covid-19+testing</a>
+              </span>
+              <h4 className='title'><a href='#'>Premières nations, Inuit et Métis</a></h4>
+              <span className='source'>Mikana et Amnistie Internationale</span>
+              <p className='description'>
+                Le terme Autochtone désigne une diversité de nations et de peuples ancrés dans les territoires qu'ils occupent depuis des millénaires.
+                Leur histoires se rejoingne...
+              </p>
+            </div>
+            <div className='ressource'>
+              <span className='media'>
+                <span className='type'>Balado</span>
+                <a href='#'>osensenparler.ca</a>
+              </span>
+              <h4 className='title'><a href='#'>Oser s'en parler</a></h4>
+              <span className='source'>Charlotte Côté</span>
+              <p className='description'>
+                Balado pour allochtones sur la réalité des peuples autochtones vivant au soit-disant Canada.
+              </p>
+            </div>
+            <div className='ressource'>
+              <span className='media'>
+                <span className='type'>Site</span>
+                <a href='#'>mikanna.ca</a>
+              </span>
+              <h4 className='title'><a href='#'>Mikana</a></h4>
+              <p className='description'>
+                Site oeuvrant à la sensibilisation sur les enjeux autochtones.
+              </p>
+            </div>
+            <div className='ressource'>
+              <span className='media'>
+                <span className='type'>Lexique</span>
+                <a href='#'>osensenparler.ca/covid-19+testing</a>
+              </span>
+              <h4 className='title'><a href='#'>Premières nations, Inuit et Métis</a></h4>
+              <span className='source'>Mikana et Amnistie Internationale</span>
+              <p className='description'>
+                Le terme Autochtone désigne une diversité de nations et de peuples ancrés dans les territoires qu'ils occupent depuis des millénaires.
+                Leur histoires se rejoingne...
+              </p>
+            </div>
+            <div className='ressource'>
+              <span className='media'>
+                <span className='type'>Balado</span>
+                <a href='#'>osensenparler.ca</a>
+              </span>
+              <h4 className='title'><a href='#'>Oser s'en parler</a></h4>
+              <span className='source'>Charlotte Côté</span>
+              <p className='description'>
+                Balado pour allochtones sur la réalité des peuples autochtones vivant au soit-disant Canada.
+              </p>
+            </div>
+            <div className='ressource'>
+              <span className='media'>
+                <span className='type'>Site</span>
+                <a href='#'>mikanna.ca</a>
+              </span>
+              <h4 className='title'><a href='#'>Mikana</a></h4>
+              <p className='description'>
+                Site oeuvrant à la sensibilisation sur les enjeux autochtones.
+              </p>
+            </div>
+          </nav>
         </main>
+        <footer>
+          <span className='copyright'>Copyright © 2021 Jeutisc</span>
+        </footer>
       </>
     )
 }
