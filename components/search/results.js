036cd1021f6f23ca385cd97d7135b36ede1a992a
Hugo <dev@lexugo.ca>
2021-11-07 10:54:28 -0500

    CRi · Lonely Romance
    - Dark theme resources
    - Fix brand not sticking properly

--- components/search/results.js
+++ components/search/results.js
@@ -5,6 +5,7 @@ import Result from 'components/search/result'
 function Results({ hits }) {
 	return (
 		<div className="resources">
+			<p className='count'>We found {hits.length} resources:</p>
 			{ hits.map(({ objectID: id, ...props }) => <Result key={id} {...props} />) }
 		</div>
 	)
