c07fd3dde18ce1cdd9bb931ac8aee7009138baa6
Hugo <dev@lexugo.ca>
2021-12-26 18:06:58 -0500

    Little Simz · Rollin Stone
    - Support subcategories

--- components/search/by/subcategory.js
+++ components/search/by/subcategory.js
@@ -1,10 +1,9 @@
 import useParam from 'hooks/useParam'
 import { useSubcategories } from 'hooks/useSearch'
-import Link from '../../link'
-import { Configure } from 'react-instantsearch-dom'
 
-export default function Subcategories() {
-	const [category] = useParam('category')
+import Link from 'components/link'
+
+export default function Subcategories({ currentRefinement: refinement }) {
 	const subcategories = useSubcategories()
 
 	return (
@@ -12,18 +11,18 @@ export default function Subcategories() {
             <p>More specifically, I want to learn about</p>
 			<div className='subcategories'>
 				{ subcategories.map(category =>
-					<Subcategory key={category} label={category} onClick={() => console.log('click')} />)
+					<Subcategory key={category} label={category} refined={category === refinement} />)
 				}
 			</div>
 		</div>
 	)
 }
 
-function Subcategory({ label }) {
+function Subcategory({ label, refined }) {
 	const [category] = useParam('category') // Todo: refactor category links
 
 	return (
-		<Link href={`${category}/${label}`} className='subcategory' >
+		<Link href={`/${category}/${label}`} className={(refined ? 'refined ' : '') + 'subcategory' } >
 			<h3 className='title'>{ label }</h3>
 			<p className='description'>Facilis harum impedit maxime molestiae rem reprehenderit suscipit veniam.</p>
 		</Link>
