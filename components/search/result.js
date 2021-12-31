b58af8f341b3b8ddd1fcd016c098d301b36235d8
Hugo <dev@lexugo.ca>
2021-12-30 20:31:58 -0500

    The Orielles · Come Down On Jupiter
    - Toggle resource details on click

--- components/search/result.js
+++ components/search/result.js
@@ -1,14 +1,26 @@
-export default function Result({ title, subtitle, reference, type, source, description }) {
+import useToggle from 'hooks/useToggle'
 
+export default function Result({ title, subtitle, reference, type, source, description }) {
+	const [detailed, toggleDetailed] = useToggle()
+
+	function handleToggle(e) {
+		if (!e.target.href) // Don't toggle on links
+			toggleDetailed()
+	}
+
+	const detailedClass = description ? 'detailed ' : ''
 	const mediaType = Array.isArray(type) ? type[0] : type // Only shoe the first media type
 	return (
-		<div className={`${mediaType} resource`}>
+		<div className={`${detailedClass}resource`} onClick={handleToggle}>
 			<span className='media'>
 				<span className='type'>{ mediaType }</span>
 				<a href={reference} target='_blank' className='reference'>{ reference }</a>
 			</span>
+
 			<h4 className='title'>{ reference ? <a href={ reference } target='_blank'>{ title }</a> : title }</h4>
 			{ subtitle && <p className='description'>{ subtitle }</p> }
+
+			{ detailed && description && <p className='details'>{ description }</p> }
 		</div>
 	)
 }
